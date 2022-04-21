faa61991f4b52a482329459e1607a35794dc314d
xanedesu <xanedesu@gmail.com>
2022-04-21 22:23:53 +0300

    helmet, cors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-// import helmet from 'helmet';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,11 +9,11 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
 
-  // app.enableCors({
-  //   origin: '*',
-  // });
+  app.enableCors({
+    origin: '*',
+  });
 
-  // app.use(helmet());
+  app.use(helmet());
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
