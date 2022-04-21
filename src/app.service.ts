067974416fa4bce7493888b15eacee292f4027a4
xanedesu <xanedesu@gmail.com>
2022-04-22 00:26:43 +0300

    Пояснения к тому, как работать с чатботами

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 import { PubSub } from 'graphql-subscriptions';
+import { WebhookEventType } from './webhook/enums/webhook-event-type.enum';
 
 export const pubSub = new PubSub();
 
@@ -14,15 +15,31 @@ export class AppService {
   }
 
   handleEvents(projectId: number, event: any) {
+    // TODO: обмен сообщениями с сервисом ботов
+    // после общения с ботом отправлять особый тип события
+    // со своим набором данных.
+    // На основе полученных данных принимать решение чтто делать с контактом и чатом.
+    // 1 создать контакт и поместить в очередь
+    // 2 создать контакт и поместить в решенные
+    // if (???) {
+    //     отправка запроса на бэкенд ботов
+    //     в зависимости от ответа
+    //     1 не делать ничего (return)
+    //     что-то из вышенаписанного списка
+    // }
+
     switch (event.type) {
-      case 'IncomingChats':
+      case WebhookEventType.IncomingChats:
         this.handleChats(projectId, event.payload).catch(() => null);
         break;
 
-      case 'IncomingMessages':
-      case 'OutgoingMessages':
+      case WebhookEventType.IncomingMessages:
+      case WebhookEventType.OutgoingMessages:
         this.handleMessages(projectId, event.payload);
         break;
+
+      // case 'событие от бота':
+      //   break
     }
   }
 
