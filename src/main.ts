08046f1be0eda640c2ae5d8dd94a75462029ced3
xanedesu <xanedesu@gmail.com>
2022-06-18 01:22:06 +0300

    fastify

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { graphqlUploadExpress } from 'graphql-upload';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   const prismaServive = app.get(PrismaService);
   await prismaServive.enableShutdownHooks(app);
