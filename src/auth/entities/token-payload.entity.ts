f146ebedb990c8700ef429d84c42342b062b43d3
xanedesu <xanedesu@gmail.com>
2022-12-06 22:19:31 +0300

    Добавил type?: AssigneeType;

--- src/auth/entities/token-payload.entity.ts
+++ src/auth/entities/token-payload.entity.ts
@@ -1,8 +1,10 @@
+import { AssigneeType } from 'src/contact/enums/assignee-type.enum';
 import { Billing } from 'src/project/entities/billing.entity';
 import { Role } from 'src/project/entities/role.entity';
 
 export class TokenPayload {
   id: number;
+  type?: AssigneeType;
   project?: {
     id: number;
     billing: Billing;
